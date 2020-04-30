137c09e7d3c7716393d0c81dbdfd2cd4173d4a42
viwolf <viwolf2020@gmail.com>
2020-04-30 08:28:43 -0600

    comienzo bootstrap

--- Viwolf.Rentals.Solutions/Viwolf.Rentals.FrontEnd.Web/App_Start/BundleConfig.cs
+++ Viwolf.Rentals.Solutions/Viwolf.Rentals.FrontEnd.Web/App_Start/BundleConfig.cs
@@ -19,12 +19,65 @@ namespace Viwolf.Rentals.FrontEnd.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+
+
+           bundles.Add(new ScriptBundle("~/bundles/css").Include(
+                     "~/Areas/Reservacion/Content/Bootstrap/assets/css/lib/weather-icons.css",
+                     "~/Areas/Reservacion/Content/~/Areas/Reservacion/Content/Bootstrap/assets/css/lib/owl.carousel.min.css",
+                     "~/Areas/Reservacion/Content/~/Areas/Reservacion/Content/Bootstrap/assets/css/lib/owl.theme.default.min.css",
+                     "~/Areas/Reservacion/Content/~/Areas/Reservacion/Content/Bootstrap/assets/css/lib/font-awesome.min.css",
+                     "~/Areas/Reservacion/Content/~/Areas/Reservacion/Content/Bootstrap/assets/css/lib/themify-icons.css",
+                     "~/Areas/Reservacion/Content/~/Areas/Reservacion/Content/Bootstrap/assets/css/lib/menubar/sidebar.css",
+                     "~/Areas/Reservacion/Content/~/Areas/Reservacion/Content/Bootstrap/assets/css/lib/bootstrap.min.css",
+                     "~/Areas/Reservacion/Content/~/Areas/Reservacion/Content/Bootstrap/assets/css/lib/helper.css",
+                     "~/Areas/Reservacion/Content/~/Areas/Reservacion/Content/Bootstrap/assets/css/style.css"));
+
+
+
+          
+                                                bundles.Add(new StyleBundle("~/bundles/js").Include(
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/jquery.min.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/jquery.nanoscroller.min.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/menubar/sidebar.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/preloader/pace.min.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/bootstrap.min.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/circle-progress/circle-progress.min.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/circle-progress/circle-progress-init.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/morris-chart/raphael-min.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/morris-chart/morris.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/morris-chart/morris-init.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/flot-chart/jquery.flot.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/flot-chart/jquery.flot.resize.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/flot-chart/flot-chart-init.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/jquery.vmap.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/jquery.vmap.min.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/jquery.vmap.sampledata.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.world.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.algeria.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.argentina.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.brazil.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.france.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.germany.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.greece.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.iran.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.iraq.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.russia.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.tunisia.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.europe.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/country/jquery.vmap.usa.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/vector-map/vector.init.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/weather/jquery.simpleWeather.min.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/weather/weather-init.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/owl-carousel/owl.carousel.min.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/lib/owl-carousel/owl.carousel-init.js",
+                      "~/Areas/Reservacion/Content/Bootstrap/assets/js/scripts.js"));
         }
     }
 }
