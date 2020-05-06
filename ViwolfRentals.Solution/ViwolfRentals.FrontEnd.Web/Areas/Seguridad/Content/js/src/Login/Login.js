6e0c413422b0583f7874c832a23e3745cd5413dc
viwolf <viwolf2020@gmail.com>
2020-05-06 15:23:17 -0600

    Login

---
+++ ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/Areas/Seguridad/Content/js/src/Login/Login.js
@@ -0,0 +1,17 @@
+﻿var Login = function () {
+    var btnLogin = $("#btnEntrar");
+
+    (function ($) {
+        init
+    })
+
+    var init = function () {
+        debugger;
+        btnLogin.click(fnLogin);
+    }
+
+    var fnLogin = function (e) {
+        debugger;
+        alert("Bienvenido");
+    }
+}();
