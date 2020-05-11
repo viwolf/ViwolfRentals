98f9153016fed1f462ab089df49570a87b8ac5ce
viwolf <viwolf2020@gmail.com>
2020-05-11 08:49:03 -0600

    Se crea combo de proveedores

--- ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/Areas/Viwolf/Content/js/src/Reservaciones/Reservaciones.js
+++ ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/Areas/Viwolf/Content/js/src/Reservaciones/Reservaciones.js
@@ -16,31 +16,122 @@
     var txtPlaca = $("#txtPlaca");
     var txtFechaInicio = $("#txtFechaInicio");
     var txtFechaFinal = ("#txtFechaFinal");
-
-
-
     var btnGuardar = $("#btnGuardar");
 
     var Init = function () {
         txtSurfRacks.change(cambiarEstadoSurfRacks);
         txtCuentaCobrar.change(cambiarEstadoProveedor);
-       
-        //cargarSelect2(txtEntregaHotel,
-        //    {
-        //        PlaceHolder: "Seleccione Estado",
-        //        minimumResultsForSearch: Infinity,
-        //        Url: "Vouchers/ListarEstadoVoucher",
-        //        DataType: 'json',
-        //        Type: "POST",
-        //        Id: "IdEstadoVoucher",
-        //        Text: "Nombre",
-        //        SuccessFunction: function (data) {
-        //            estado = data.Data;
-        //        }
-        //    });
+        debugger;
+        cargarSelect2(txtProveedor,
+            {
+                PlaceHolder: "Seleccione Proveedor",
+                minimumResultsForSearch: Infinity,
+                Url: "Proveedores/ListarProveedores",
+                DataType: 'json',
+                Type: "POST",
+                Id: "IdProveedor",
+                Text: "NombreProveedor",
+                SuccessFunction: function (data) {
+                    estado = data.Data;
+                }
+            });
 
         btnGuardar.click(fnGuardarReservacion);
     }
+
+    var cargarSelect2 = function (elemento, configuracion) {
+        debugger;
+        $.ajax({
+            type: "POST",
+            url: "Proveedores/ListarProveedores",
+            data: "{}",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (msg) {
+                debugger;
+                //var datos = $.parseJSON(msg.Data);
+                $(msg.Data).each(function () {
+                    debugger;
+                    var option = $(document.createElement('option'));
+
+                    option.text(this.NombreProveedor);
+                    option.val(this.IdProveedor);
+
+                    $("#txtProveedor").append(option);
+                });
+
+                //$(datos).each(function () {
+                //    var option = $(document.createElement('option'));
+
+                //    option.text(this.IdProveedor);
+                //    option.val(this.NombreProveedor);
+
+                //    $("#txtProveedor").append(option);
+                //});
+            },
+            error: function (msg) {
+                $("#dvAlerta > span").text("Error al llenar el combo");
+            }
+        });
+
+        //debugger
+        //elemento.select2({
+        //    cacheDataSource: [],
+        //    placeholder: configuracion.PlaceHolder,
+        //    multiple: configuracion.Multiple,
+        //    query: function (query) {
+        //        debugger;
+        //        self = this;
+        //        var key = query.term;
+        //        var cachedData = self.cacheDataSource[key];
+        //        if (cachedData) {
+        //            query.callback({
+        //                results: cachedData
+        //            });
+        //            return;
+        //        } else {
+        //            debugger;
+        //            $.ajax({
+        //                url: configuracion.Url,
+        //                data: configuracion.Data,
+        //                dataType: 'json',
+        //                type: 'POST',
+        //                success: function (serverData) {
+        //                    if (configuracion.SuccessFunction) {
+        //                        configuracion.SuccessFunction(serverData);
+        //                    }
+        //                    var data = {
+        //                        results: []
+        //                    };
+        //                    $.each(serverData.Data, function () {
+        //                        data.results.push({
+        //                            id: this[configuracion.Id], text: this[configuracion.Text]
+        //                        });
+        //                    });
+        //                    self.cacheDataSource[key] = data.results;
+        //                    results = data.results;
+        //                    query.callback(data);
+        //                }
+        //            })
+        //        }
+        //    },
+
+        //    initSelection: configuracion.InitSelection,
+        //    dropdownCssClass: "bigdrop", // apply css that makes the dropdown taller
+        //    escapeMarkup: function (m) {
+        //        return m;
+        //    } // we do not want to escape markup since we are displaying html in results
+        //});
+
+
+
+
+
+
+    };
+
+
+  
 
     function cambiarEstadoSurfRacks() {
         debugger;
