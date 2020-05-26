618c89533f929000dd80e8526aeb82040cd999d5
viwolf <viwolf2020@gmail.com>
2020-05-26 09:46:50 -0600

    bundle script seguridad

--- ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/App_Start/BundleConfig.cs
+++ ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/App_Start/BundleConfig.cs
@@ -8,14 +8,20 @@ namespace ViwolfRentals.FrontEnd.Web
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-
-
-            bundles.Add(new StyleBundle("~/Content/Plugins")
-                .Include("~/Content/Plugins/datatables/jquery-3.5.1.js")
-            .Include("~/Content/Plugins/datatables/jquery.dataTables.min.js")
-            .Include("~/Content/Plugins/datatables/dataTables.bootstrap.min.js")
-            .Include("~/Content/Plugins/datatables/dataTables.select.min.js")
-            );
+            bundles.IgnoreList.Clear();
+            bundles.UseCdn = true;
+            bundles.Add(
+                new ScriptBundle("~/bundles/Seguridad")
+                    .Include("~/Areas/Seguridad/Content/Vendor/jquery/jquery-3.2.1.min.js")
+                    .Include("~/Areas/Seguridad/Content/Vendor/bootstrap/js/popper.js")
+                    .Include("~/Areas/Seguridad/Content/Vendor/bootstrap/js/bootstrap.min.js")
+                    .Include("~/Areas/Seguridad/Content/Vendor/select2/select2.min.js")
+                    .Include("~/Areas/Seguridad/Content/Vendor/tilt/tilt.jquery.min.js")
+                    .Include("~/Content/Plugins/ajaxloader/ajaxloader.js")
+                    .Include("~/Content/Plugins/bootstrap/js/bootstrap.min.js")
+                    .Include("~/Areas/Seguridad/Content/Plugin/ModalMessage/popper.min.js")
+                    .Include("~/Areas/Seguridad/Content/Plugin/ModalMessage/bootstrap.min.js")
+                    .Include("~/Content/Plugins/bootstrap-dialog/js/bootstrap-dialog.js"));
         }
     }
 }
