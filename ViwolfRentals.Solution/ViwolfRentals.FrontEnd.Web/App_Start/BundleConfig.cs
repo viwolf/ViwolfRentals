8a6e6d8c5448389faf209318e0e22da7cd8a0d1c
viwolf <viwolf2020@gmail.com>
2020-05-27 10:32:03 -0600

    bundle Reservaciones

--- ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/App_Start/BundleConfig.cs
+++ ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/App_Start/BundleConfig.cs
@@ -19,8 +19,8 @@ namespace ViwolfRentals.FrontEnd.Web
                     .Include("~/Areas/Seguridad/Content/Vendor/tilt/tilt.jquery.min.js")
                     .Include("~/Content/Plugins/ajaxloader/ajaxloader.js")
                     .Include("~/Content/Plugins/bootstrap/js/bootstrap.min.js")
-                    .Include("~/Areas/Seguridad/Content/Plugin/ModalMessage/popper.min.js")
-                    .Include("~/Areas/Seguridad/Content/Plugin/ModalMessage/bootstrap.min.js")
+                    .Include("~/Content/Plugins/ModalMessage/popper.min.js")
+                    .Include("~/Content/Plugins/ModalMessage/bootstrap.min.js")
                     .Include("~/Content/Plugins/bootstrap-dialog/js/bootstrap-dialog.js"));
             bundles.Add(
                new ScriptBundle("~/bundles/ViwolfRentals")
@@ -29,20 +29,26 @@ namespace ViwolfRentals.FrontEnd.Web
                     .Include("~/Areas/Viwolf/Content/dist/js/bootstrap.bundle.min.js")
                     .Include("~/Areas/Viwolf/Content/dist/js/scripts.js")
                     .Include("~/Areas/Viwolf/Content/dist/js/jquery.dataTables.min.js")
-                    .Include("~/Areas/Viwolf/Content/dist/js/jquery/dataTables.bootstrap4.min.js")
-                    //.Include("~/Areas/Viwolf/Content/dist/js/jquery/jquery-1.12.4.js")
-                    //.Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/js/jquery-ui.js")
-                    //.Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/select2/select2.min.js")
-                    //.Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/datepicker/moment.min.js")
-                    //.Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/datepicker/daterangepicker.js")
-                    //.Include("~/Content/Plugins/ajaxloader/ajaxloader.js")
-                    //.Include("~/Content/js/src/Dialog.js")
-                    //.Include("~/Areas/Seguridad/Content/Plugin/ModalMessage/popper.min.js")
-                    //.Include("~/Areas/Seguridad/Content/Plugin/ModalMessage/bootstrap.min.js")
-                    //.Include("~/Areas/Viwolf/Content/dist/js/jquery.timepicker.min.js")
+                    //.Include("~/Areas/Viwolf/Content/dist/js/jquery/dataTables.bootstrap4.min.js")
+                    .Include("~/Areas/Viwolf/Content/images/engine1/wowslider.js")
                     );
 
-            bundles.Add(new StyleBundle("~/bundles/Estilo/Seguridad")
+            bundles.Add(
+              new ScriptBundle("~/bundles/ViwolfRentalsReservaciones")
+                .Include("~/Areas/Viwolf/Content/dist/js/jquery/jquery-1.12.4.js")
+                .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/js/jquery-ui.js")
+                .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/select2/select2.min.js")
+                .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/datepicker/moment.min.js")
+                .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/datepicker/daterangepicker.js")
+                .Include("~/Content/Plugins/ajaxloader/ajaxloader.js")
+                .Include("~/Content/js/src/Dialog.js")
+                .Include("~/Content/Plugins/ModalMessage/popper.min.js")
+                .Include("~/Content/Plugins/ModalMessage/bootstrap.min.js")
+                .Include("~/Areas/Viwolf/Content/dist/js/jquery.timepicker.min.js")
+                );
+
+
+           bundles.Add(new StyleBundle("~/bundles/Estilo/Seguridad")
               .Include("~/Areas/Seguridad/Content/images/icons/favicon.ico")
               .Include("~/Areas/Seguridad/Content/vendor/bootstrap/css/bootstrap.min.css")
               .Include("~/Areas/Seguridad/Content/fonts/font-awesome-4.7.0/css/font-awesome.min.css")
@@ -51,11 +57,17 @@ namespace ViwolfRentals.FrontEnd.Web
 
             bundles.Add(new StyleBundle("~/bundles/Estilo/ViwolfRentals")
               .Include("~/Areas/Viwolf/Content/dist/css/styles.css")
-              .Include("~/Areas/Viwolf/Content/dist/js/jquery/dataTables.bootstrap4.min.js")
               );
 
+            bundles.Add(new StyleBundle("~/bundles/Estilo/ViwolfRentalsReservaciones")
+             .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/mdi-font/css/material-design-iconic-font.min.css")
+             .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/font-awesome-4.7/css/font-awesome.min.css")
+             .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/css/fonts.googleapis.css")
+             .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/select2/select2.min.css")
+             .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/vendor/datepicker/daterangepicker.css")
+             .Include("~/Areas/Viwolf/Content/js/src/Reservaciones/Content/css/main.css")
+             );
 
-           
         }
     }
 }
