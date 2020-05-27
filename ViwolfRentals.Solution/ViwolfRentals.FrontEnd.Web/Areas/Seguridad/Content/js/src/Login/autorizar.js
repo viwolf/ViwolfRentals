e748e3560cfb84a7ccb471bb310f37f3451a3704
viwolf <viwolf2020@gmail.com>
2020-05-27 16:09:58 -0600

    creacion ventana autorizar

---
+++ ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/Areas/Seguridad/Content/js/src/Login/autorizar.js
@@ -0,0 +1,17 @@
+﻿var autorizar = function () {
+    var modalAutorizar = $('#popupAutorizarOperacion');
+
+    var fnInit = function () {
+        modalAutorizar.modal('show');
+    }
+
+    var fnAbrirModal = function () {
+
+        fnInit();
+    };
+
+    return {
+        AbrirModal: fnAbrirModal
+    }
+}();
+
