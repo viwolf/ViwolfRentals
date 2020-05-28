1dc49abc0f0cb3261329a9998200f1013f0abc05
viwolf <viwolf2020@gmail.com>
2020-05-28 13:47:27 -0600

    creacion del modal de autorizacion

---
+++ ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/Areas/Seguridad/Content/js/src/Login/Autorizacion.js
@@ -0,0 +1,31 @@
+﻿var autorizacionLogin = function () {
+    var modalLogin = $('#GetOfferModal'); //$('#GetOfferModal'); 
+    var form = $("#formGetOffer");
+    var btnAutorizar = $("#formGetOffer");
+
+    var fnInit = function () {
+        btnAutorizar.unbind().click(fnAutorizar);
+    };
+
+    var fnAutorizar = function () {
+        if (form[0].checkValidity() === false) {
+            debugger;
+            event.preventDefault()
+            event.stopPropagation()
+
+        }
+
+        form.addClass('was-validated');
+    }
+
+    var fnAbrirModal = function () {
+        //fnCallbak = callback;
+        fnInit();
+        modalLogin.modal('show');
+        //fnBuscarVehiculo();
+    };
+
+    return {
+        AbrirModal: fnAbrirModal
+    }
+}();
