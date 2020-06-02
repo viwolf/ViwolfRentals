cca45e30de997f07aeb8faf290c03e3049a2b38e
viwolf <viwolf2020@gmail.com>
2020-06-02 14:17:20 -0600

    Generacion de calendario de reservas

--- ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/Areas/Viwolf/Content/js/src/Reservaciones/Calendario.js
+++ ViwolfRentals.Solution/ViwolfRentals.FrontEnd.Web/Areas/Viwolf/Content/js/src/Reservaciones/Calendario.js
@@ -1,21 +1,60 @@
 ﻿var calendarioReservas = function () {
-    var calendarEl = $("#calendar");
+    var txtAnno = $("#txtAnno");
+    var txtMes = $("#txtMes");
+    var btnMostrarCalendario = $("#btnMostrarCalendario");
+    var numeroDias = 0;
+    
 
-    var fnCargarCalendario = function () {
-       
+    var generarClendario = function () {
 
-       
+
+
+        let myTable = "<table id='tblCalendario'><tr><td style='width: 100px; border:1px solid black;border-collapse:collapse;'>Placa</td>";
+        for (var i = 1; i <= numeroDias; i++) {
+            myTable += "<td style='width: 100px; color: red; text-align: center; border:1px solid black;border-collapse:collapse;'>" + i + "</td>";
+        }
+
+        myTable += "<tr><td style='width: 100px;text-align: center;border:1px solid black;border-collapse:collapse;'>Placa</td>";
+        for (let i = 1; i <= numeroDias; i++) {
+            myTable += "<td style='width: 100px;text-align: center; border:1px solid black;border-collapse:collapse; background-color:#FF0000 ';></td>";
+        }
+        myTable += "</tr>";
+
+
+        myTable += "</table>";
+        document.getElementById('tblData').innerHTML = myTable;
+    }
+
+
+    var fnMostrarCalendario = function () {
+        debugger;
+        numeroDias = diasEnUnMes(txtMes.val(), txtAnno.val());
+        generarClendario();
+
+    }
+
+    var diasEnUnMes = function (mes, año) {
+        return new Date(año, mes, 0).getDate();
+    }
+    var fncargarComboAnno = function () {
+        var d = new Date();
+        var n = d.getFullYear();
+        var select = document.getElementById("txtAnno");
+        for (var i = n; i >= 2000; i--) {
+            var opc = document.createElement("option");
+            opc.text = i;
+            opc.value = i;
+            select.add(opc)
+        }
+    }
+
+    var fnInit = function () {
+        fncargarComboAnno();
+        btnMostrarCalendario.click(fnMostrarCalendario);
     };
 
-    //var fnInit = function () {
-       
-    //};
+    $(function () {
+        fnInit();
+    });
 
-    //$(function () {
-    //    fnInit();
-    //});
-
-    return {
-        CargarCalendario: fnCargarCalendario
-    }
 }();
