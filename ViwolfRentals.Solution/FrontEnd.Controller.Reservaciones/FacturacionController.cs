73a80059c5db948618ae81293370a36b35d18805
viwolf <viwolf2020@gmail.com>
2020-06-23 12:41:03 -0600

    Se crea pantalla de facturacion

---
+++ ViwolfRentals.Solution/FrontEnd.Controller.Reservaciones/FacturacionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FrontEnd.Controllers.Viwolf
+{
+    public class FacturacionController:Controller
+    {
+        public ActionResult Index(string usuario, string idUsuario)
+        {
+            ViewBag.Usuario = usuario;
+            ViewBag.IdUsuario = idUsuario;
+           
+            return View();
+        }
+    }
+}
